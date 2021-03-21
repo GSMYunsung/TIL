4ca209bc3f393212d692a2f31149cd6ddfce1df3
choiyunsung_100 <12345ggh81@gmail.com>
2021-03-21 18:22:37 +0900

    calculating_machine_test

---
+++ app/src/main/java/Android/calculating_machine.kt
@@ -0,0 +1,12 @@
+package Android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class calculating_machine : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculating_machine)
+    }
+}
